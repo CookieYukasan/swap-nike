4ca0845aaa5fcde68ef7a6f1425de7dfb0ce0d6d
Adson Martins <adson.martins982@gmail.com>
2023-05-21 07:52:48 -0300

    yuupi

--- src/utils/fetchWrapper.ts
+++ src/utils/fetchWrapper.ts
@@ -2,5 +2,5 @@ export async function fetchWrapper<T = unknown>(input: RequestInfo | URL, init?:
   const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}${input}`, init)
   const result = await response.json()
   
-  return result.data ? result.data : result as T
+  return result as T
 }
